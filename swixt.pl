e7039b34105e2b6e0fd8c1f7cda322b39f0067d4
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2024-08-14 19:06:19 +0300

    Use parameters in query

--- swixt.pl
+++ swixt.pl
@@ -1,4 +1,5 @@
 :- module(swixt, [q/2, q/1, insert/1, insert/2, status/1]).
+:- use_module(library(yall)).
 :- use_module(library(apply)).
 :- use_module(library(http/http_open)).
 :- use_module(library(http/http_client)).
@@ -12,8 +13,9 @@ xt_post(Path, Json, Results) :-
     http_open(Url, Stream, [post(json(Json))]),
     json_read_dict(Stream, Results, [tag('@type'),default_tag(data)]).
 
-query(Sql, Results) :-
-    xt_post(query, d{sql: Sql}, Results).
+query(Sql, Args, Results) :-
+    args_list(Args, ArgsList),
+    xt_post(query, d{sql: Sql, queryOpts: d{args: ArgsList}}, Results).
 
 to_arg_ref(N,Ref) :- format(atom(Ref), '$~d', [N]).
 
@@ -33,10 +35,32 @@ status(Status) :-
     http_open(Url, Stream, []),
     json_read_dict(Stream, Status, [tag('@type'),default_tag(xt)]).
 
-where([], ''). % no fields, don't emit a WHERE clause
-where(Fields, FormattedWhereClause) :-
+%%%%%
+%% Structure to build up args, compound term keeps track of current
+%% arg number and a list of args.
+%% args_list turns the argument structure into a list.
+
+empty_args(args(1,[])).
+args_list(args(_,Args), ArgsRev) :- reverse(Args, ArgsRev).
+arg(ArgVal, ArgRef, args(Cur, Args), args(Next, [ArgVal|Args])) :-
+    succ(Cur, Next),
+    to_arg_ref(Cur, ArgRef).
+
+
+
+%% Partition fields into regular and special control fields
+partition_fields(FieldsIn, ClauseFields, SpecialFields) :-
+    partition(special_field_pair, FieldsIn, SpecialFields, ClauseFields).
+
+%%%%%%%
+%% Where clause generation
+
+where([], '', Args, Args). % no fields, don't emit a WHERE clause
+where(Fields, FormattedWhereClause, ArgsIn, ArgsOut) :-
     length(Fields, Len), Len > 0,
-    maplist(format_where, Fields, FieldClauses),
+    foldl([Field, SQL0^Args0, [Out|SQL0]^Args1]>>format_where(Field, Out, Args0, Args1),
+          Fields, []^ArgsIn, SQLs^ArgsOut),
+    reverse(SQLs, FieldClauses),
     atomic_list_concat(FieldClauses, ' AND ', FieldClausesJoined),
     format(atom(FormattedWhereClause), ' WHERE ~w', [FieldClausesJoined]).
 
@@ -44,25 +68,33 @@ special_field('_only').
 special_field('_order').
 special_field_pair(Field-_) :- special_field(Field).
 
-%% Partition fields into regular and special control fields
-partition_fields(FieldsIn, ClauseFields, SpecialFields) :-
-    partition(special_field_pair, FieldsIn, SpecialFields, ClauseFields).
 
-format_where(Field- >(V), Out) :-
-    format(atom(Out), '~w > ~w', [Field, V]).
-format_where(Field- <(V), Out) :-
-    format(atom(Out), '~w < ~w', [Field, V]).
-format_where(Field-between(Min,Max), Out) :-
-    format(atom(Out), '(~w BETWEEN ~w AND ~w)', [Field, Min, Max]).
-format_where(Field-like(Str), Out) :-
-    %%% FIXME: have query PARAMETERS instead of strings mashed together
-    format(atom(Out), '~w LIKE ''~w''', [Field,Str]).
-format_where(Field-not(Clause), Out) :-
-    format_where(Field-Clause, Where),
-    format(atom(Out), 'NOT (~w)', [Where]).
-format_where(Field-Val, Out) :-
+
+format_where_op(Field, Op, Arg, Out, Args, Args) :-
+    atom(Arg),
+    % reference another field, not a parameter
+    format(atom(Out), '~w ~w ~w', [Field, Op, Arg]).
+
+format_where_op(Field, Op, Arg, Out, ArgsIn, ArgsOut) :-
+    \+ atom(Arg),
+    arg(Arg, Ref, ArgsIn, ArgsOut),
+    format(atom(Out), '~w ~w ~w', [Field, Op, Ref]).
+
+format_where(Field- >(V), Out, ArgsIn, ArgsOut) :-format_where_op(Field, '>', V, Out, ArgsIn, ArgsOut).
+format_where(Field- <(V), Out, ArgsIn, ArgsOut) :-format_where_op(Field, '<', V, Out, ArgsIn, ArgsOut).
+format_where(Field-Val, Out, ArgsIn, ArgsOut) :-
     \+ compound(Val),
-    format(atom(Out), '~w = ~w', [Field, Val]).
+    format_where_op(Field, '=', Val, Out, ArgsIn, ArgsOut).
+format_where(Field-between(Min,Max), Out, ArgsIn, ArgsOut) :-
+    arg(Min, MinRef, ArgsIn, Args1),
+    arg(Max, MaxRef, Args1, ArgsOut),
+    format(atom(Out), '(~w BETWEEN ~w AND ~w)', [Field, MinRef, MaxRef]).
+format_where(Field-like(Str), Out, ArgsIn, ArgsOut) :- format_where_op(Field, 'LIKE', Str, Out, ArgsIn, ArgsOut).
+
+format_where(Field-not(Clause), Out, ArgsIn, ArgsOut) :-
+    format_where(Field-Clause, Where, ArgsIn, ArgsOut),
+    format(atom(Out), 'NOT (~w)', [Where]).
+
 
 %%%%%%%
 %% Ordering, create an ORDER BY clause from the special '_order' field
@@ -89,10 +121,14 @@ projection(Specials, Projection) :-
 %%%%%%%5
 %% Dict to SQL conversion
 
-dict_sql(Dict, SQL) :-
+dict_sql(Dict, SQL, Args) :-
+    empty_args(InitialArgs),
+    dict_sql(Dict, SQL, InitialArgs, Args).
+
+dict_sql(Dict, SQL, ArgsIn, ArgsOut) :-
     dict_pairs(Dict, Table, Fields),
     partition_fields(Fields, ClauseFields, SpecialFields),
-    where(ClauseFields, FormattedWhereClause),
+    where(ClauseFields, FormattedWhereClause, ArgsIn, ArgsOut),
     order_by(SpecialFields, OrderByClause),
     projection(SpecialFields, Projection),
     format(atom(SQL),
@@ -103,9 +139,10 @@ dict_sql(Dict, SQL) :-
 %% The main query interface
 
 q(Candidate, Results) :-
-    dict_sql(Candidate, SQL),
-    writeln(query(SQL)),
-    query(SQL, Results).
+    empty_args(Args0),
+    dict_sql(Candidate, SQL, Args0, Args),
+    writeln(query(SQL,Args)),
+    query(SQL, Args, Results).
 
 insert(Dict, Result) :-
     dict_pairs(Dict, Table, Fields),
