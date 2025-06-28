e7a8cd46865ae3e0820bac347c11266a441a9733
Tatu Tarvainen <tatu.tarvainen@me.com>
2025-06-28 18:03:53 +0300

    Cleanup array read

--- swixt.c
+++ swixt.c
@@ -8,13 +8,14 @@
 //#include "SWI-Stream.h"
 #include <stdbool.h>
 #include "json.h"
+#include "util.h"
 
 static foreign_t pl_connect(term_t connstr, term_t CONN) {
   char *s;
   if(PL_get_chars(connstr, &s, CVT_ALL|REP_UTF8)) {
     PgConn *conn = pg_connect(s);
     if(!conn) {
-      fprintf(stderr, "Connection failed with: %s\n", s);
+      err("Connection failed with: %s", s);
       return false;
     }
     functor_t c = PL_new_functor(PL_new_atom("xtconn"), 1);
@@ -58,7 +59,7 @@ static foreign_t pl_close(term_t conn_handle) {
 #define MAX_ARGS 32
 
 static bool from_db(term_t t, char *data, size_t len, int type) {
-  printf("from db: %zu, oid: %d\n", len, type);
+  //printf("from db: %zu, oid: %d\n", len, type);
   union {
     uint64_t int_val;
     double double_val;
@@ -82,41 +83,12 @@ static bool from_db(term_t t, char *data, size_t len, int type) {
   }
 }
 
-typedef struct Arr {
-  term_t item;
-  bool success;
-} Arr;
-
-static Arr from_db_array_(char *data, int type, size_t nitems) {
-  if (nitems == 0) {
-    return (Arr){PL_new_nil_ref(), true};
-  } else {
-    int len = ntohl(*((int32_t *)data));
-    Arr rest = from_db_array_(data + 4 + len, type, nitems - 1);
-    if(rest.success) {
-      term_t item = PL_new_term_ref();
-      if (len == -1) {
-        if(!PL_put_atom_chars(item, "nil")) goto fail;
-      } else {
-        if (!from_db(item, data+4, len, type))
-          goto fail;
-      }
-      term_t res = PL_new_term_ref();
-      if (!PL_cons_list(res, item, rest.item))
-        goto fail;
-      return (Arr) { res, true };
-    }
-  }
- fail:
-  return (Arr){0, false};
-}
-
 static bool from_db_array(term_t to, char *data) {
   // 20 byte header: ndim, has_null, element_type, dim_size, lower_bound (all
   // int32)
   int ndim = ntohl(*((int32_t *)data));
   if (ndim != 1) {
-    fprintf(stderr, "Only 1 dimensional arrays are supported at the moment, ndim: %d\n", ndim);
+    err("Only 1 dimensional arrays are supported at the moment, ndim: %d", ndim);
     return false;
   }
   int has_null = ntohl(*((int32_t *)(data + 4)));
@@ -124,22 +96,27 @@ static bool from_db_array(term_t to, char *data) {
   int dim_size = ntohl(*((int32_t *)(data + 12)));
   int lower_bound = ntohl(*((int32_t *)(data + 16)));
 
-  term_t values = PL_new_term_refs(dim_size);
-
-  Arr arr = from_db_array_(data + 20, element_type, dim_size);
-  if (arr.success) {
-    if(!PL_unify(arr.item, to)) return false;
-
-    /*printf("array ndim: %d, has_null: %d, element_type: %d, dim_size: %d, "
-           "lower_bound: %d\n",
-           ndim, has_null, element_type, dim_size, lower_bound);
-    */
-    return true;
-  } else {
-    return false;
+  term_t list = PL_copy_term_ref(to);
+  term_t item = PL_new_term_ref();
+  data += 20; // skip 20 byte header
+  for(int i=0;i<dim_size;i++) {
+    int len = ntohl(*((int32_t *)data));
+    term_t v = PL_new_term_ref();
+      if (len == -1) {
+        if(!PL_put_atom_chars(v, "nil")) goto fail;
+      } else {
+        if (!from_db(v, data+4, len, element_type))
+          goto fail;
+      }
+      if(!PL_unify_list(list, item, list) || !PL_unify(item, v))
+        goto fail;
+      data += 4 + len;
   }
-
+  return PL_unify_nil(list);
+ fail:
+  return false;
 }
+
 static bool from_db_value(term_t to, PgVal res, int type) {
   int64_t num;
   if (res.is_null) {
@@ -163,6 +140,7 @@ static bool from_db_value(term_t to, PgVal res, int type) {
 static foreign_t pl_query(term_t conn_handle, term_t query, term_t args,
                           term_t RESULT) {
 #define fail() { success = false; goto end; }
+  printf("do the query!\n");
   PgResult res;
   PgConn *conn;
   bool success;
@@ -171,45 +149,46 @@ static foreign_t pl_query(term_t conn_handle, term_t query, term_t args,
   int query_arg_types[MAX_ARGS];
   int argc = 0;
   conn = get_connection(conn_handle);
+  dbg("got connection %llu", (uint64_t) conn);
   if(conn == NULL) return false;
   if(!PL_is_list(args)) {
-    fprintf(stderr, "Query arguments is not a list\n");
+    err0("Query arguments is not a list");
     return false;
   }
   term_t arg = args;
   term_t argval = PL_new_term_ref();
   term_t arg_oid = PL_new_term_ref();
   term_t arg_str = PL_new_term_ref();
-  while(PL_is_list(arg)) {
-    if(PL_get_head(arg, argval)) {
-      if(!PL_is_compound(argval)) fail();
-      if(!PL_get_arg(1, argval, arg_oid)) fail();
-      if(!PL_get_arg(2, argval, arg_str)) fail();
-      if (PL_get_chars(arg_str, &s, CVT_ALL | REP_UTF8)) {
-        uint64_t oid;
-        if(!PL_get_uint64(arg_oid, &oid)) fail();
-        //printf("arg %d: %s (OID: %llu)\n", argc, s, oid);
-        query_args[argc] = malloc(strlen(s) + 1);
-        query_arg_types[argc] = oid;
-        strcpy((char*)query_args[argc], s);
-        argc++;
-      }
+  dbg("start adding args\n");
+  term_t head = PL_new_term_ref();
+  term_t tail = PL_copy_term_ref(args);
+  while(PL_get_list_ex(tail, head, tail)) {
+    if(!PL_is_compound(head)) fail();
+    if(!PL_get_arg(1, head, arg_oid)) fail();
+    if(!PL_get_arg(2, head, arg_str)) fail();
+    if(!PL_get_chars(arg_str, &s, CVT_ALL | REP_UTF8)) fail();
 
-    }
-    if(!PL_get_tail(arg, arg)) break;
+    uint64_t oid;
+    if(!PL_get_uint64(arg_oid, &oid)) fail();
+    dbg("arg %d: %s (OID: %llu)", argc, s, oid);
+    query_args[argc] = malloc(strlen(s) + 1);
+    query_arg_types[argc] = oid;
+    strcpy((char*)query_args[argc], s);
+    argc++;
   }
+  printf("done with args\n");
   if(PL_get_chars(query, &s, CVT_ALL|REP_UTF8)) {
-    //printf("thread(%d) running query: %s (argc: %d)\n", PL_thread_self(), s,
-    //     argc);
+    dbg("thread(%d) running query: %s (argc: %d)", PL_thread_self(), s,
+         argc);
     res = pg_query(conn, s, argc, query_arg_types, query_args);
     if(!res.success) fail();
 
     if(res.fields) {
 
-      //printf("pq result ok! %d\n", PQntuples(res));
+      dbg("pq result ok!\n");
       size_t nfields = res.fields;
 
-      term_t result = RESULT; //PL_copy_term_ref(RESULT);
+      term_t result = PL_copy_term_ref(RESULT);
 
       size_t tag_field = -1;
       atom_t table_tag = 0;
@@ -223,7 +202,6 @@ static foreign_t pl_query(term_t conn_handle, term_t query, term_t args,
           tag_field = f;
         } else {
           field_tags[f] = PL_new_atom(name);
-          //printf("field %ld type is %d\n", f, PQftype(res, f));
         }
       }
       PgRow row = pg_next_row(conn, &res);
@@ -253,7 +231,7 @@ static foreign_t pl_query(term_t conn_handle, term_t query, term_t args,
     } else if (res.success) {
       return true;
     } else {
-      fprintf(stderr, "Query failed.\n");
+      err0("Query failed.");
       fail();
     }
   }
@@ -279,45 +257,3 @@ install_t install_swixt(void) {
   PL_register_foreign("swixt_pg_query", 4, pl_query, 0);
   PL_register_foreign("swixt_json", 2, testparse, 0);
 }
-/*
-int main(int argc, char**argv) {
-  PGconn *conn;
-  PGresult *res;
-  int retval = 0;
-
-  conn = PQconnectdb("host=localhost port=5432 dbname=xtdb");
-  // Check to see that the backend connection was successfully made
-  if (PQstatus(conn) != CONNECTION_OK) {
-    fprintf(stderr, "%s", PQerrorMessage(conn));
-    retval = 1;
-    goto exit_con;
-  }
-
-  res = PQexec(conn, "SELECT * FROM customer");
-  if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-      fprintf(stderr, "SET failed: %s", PQerrorMessage(conn));
-      PQclear(res);
-      retval = 2;
-      goto exit_con;
-  }
-
-  // get all fields
-  for(int f=0; f<PQnfields(res); f++) {
-    char type[64];
-    type_name(conn, PQftype(res,f), &type[0]);
-    printf("Field: %s (type: %d => %s)\n", PQfname(res, f), PQftype(res, f), type);
-  }
-
-  // loop through results
-  for(int i=0; i<PQntuples(res); i++) {
-
-  }
-
-  PQclear(res);
-
- exit_con:
-  PQfinish(conn);
-
-  return retval;
-}
-*/
